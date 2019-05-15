f76c38abfcc846b82d4cac72c408c53e003a0eed
owlViking <50614905+owlViking@users.noreply.github.com>
2019-05-15 10:26:54 +0900

    声優リストの追加
    声優リストを作りました

--- lesson_2_itayama/lesson_2.js
+++ lesson_2_itayama/lesson_2.js
@@ -1,13 +1,8 @@
 var name= "田中太郎";
 var company= "株式会社〇〇";
-var seiyu = ["杉田智和","中村悠一","花澤香菜","雨宮天","宮野真守"];
-var key;
 
 alert(name + "は" + company + "に所属しています。");
     
-for(key in seiyu){
-    alert(seiyu[key] + "は、面白い声優です。");
-}
 
 var marvel = new Array(5);
 
---
+++ lesson_2_itayama/seiyu_list.js
@@ -0,0 +1,6 @@
+var seiyu = ["杉田智和","中村悠一","花澤香菜","雨宮天","宮野真守"];
+var key;
+
+for(key in seiyu){
+    alert(seiyu[key] + "は、面白い声優です。");
+}
