d0c45629bf1a1cf8820f784854c18c628e9e803b
owlViking <50614905+owlViking@users.noreply.github.com>
2019-05-15 09:49:14 +0900

    Update lesson_2.js
    for文をkey inに変更しました

--- lesson_2_itayama/lesson_2.js
+++ lesson_2_itayama/lesson_2.js
@@ -1,9 +1,10 @@
 var name= "田中太郎";
 var company= "株式会社〇〇";
 var seiyu = ["杉田智和","中村悠一","花澤香菜","雨宮天","宮野真守"];
+var key;
 
 alert(name + "は" + company + "に所属しています。");
     
-for(var i=0; i < 5; i++){
-    alert(seiyu[i] + "は、面白い声優です。");
+for(key in seiyu){
+    alert(seiyu[key] + "は、面白い声優です。");
 }
