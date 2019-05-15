e429187cd3b599b2861de1e84aed433b6643525f
owlViking <50614905+owlViking@users.noreply.github.com>
2019-05-15 09:42:44 +0900

    Update lesson_2.js
    声優変数を追加しました
    for文を追加しました

--- lesson_2_itayama/lesson_2.js
+++ lesson_2_itayama/lesson_2.js
@@ -1,5 +1,9 @@
 var name= "田中太郎";
 var company= "株式会社〇〇";
+var seiyu = ["杉田智和","中村悠一","花澤香菜","雨宮天","宮野真守"];
 
 alert(name + "は" + company + "に所属しています。");
     
+for(var i=0; i < 5; i++){
+    alert(seiyu[i] + "は、面白い声優です。");
+}
